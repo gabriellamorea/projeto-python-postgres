54034f55a0a34967d292e82858fec4a8be240210
Gabriel Lamorea <gablinslamorea@gmail.com>
2026-08-27 20:49:36 -0300

    feat: adiciona analises com pandas

--- analises.py
+++ analises.py
@@ -62,3 +62,62 @@ def dataframe_faturamento_produto():
     )
 
     return df
+
+def dataframe_vendas():
+    with conectar() as conexao:
+        with conexao.cursor() as cursor:
+            cursor.execute(
+                """
+                SELECT
+                    cliente,
+                    produto,
+                    vendedor,
+                    quantidade,
+                    valor_unitario,
+                    data_venda
+                FROM vendas
+                ORDER BY id;
+                """
+            )
+
+            dados = cursor.fetchall()
+            df = pd.DataFrame(
+                dados,
+                columns=[
+                    "cliente",
+                    "produto",
+                    "vendedor",
+                    "quantidade",
+                    "valor_unitario",
+                    "data_venda"
+                ]
+            )
+
+            df["faturamento"] = df["quantidade"] * df["valor_unitario"]
+
+            return df
+        
+def faturamento_total_pandas():
+    df = dataframe_vendas()
+
+    return df["faturamento"].sum()  
+
+def vendas_ordenadas_por_faturamento():
+    df = dataframe_vendas()
+
+    return df.sort_values("faturamento", ascending=False)
+
+def maior_venda():
+    df = vendas_ordenadas_por_faturamento()
+
+    return df.iloc[0]
+
+def faturamento_por_produto_pandas():
+    df = dataframe_vendas()
+
+    return df.groupby("produto")["faturamento"].sum()
+
+def faturamento_por_vendedor_pandas():
+    df = dataframe_vendas()
+
+    return df.groupby("vendedor")["faturamento"].sum()
--- tests/test_analises.py
+++ tests/test_analises.py
@@ -1,7 +1,6 @@
+from analises import faturamento_total, faturamento_por_produto, faturamento_por_vendedor, faturamento_total_pandas, maior_venda, faturamento_por_produto_pandas, faturamento_por_vendedor_pandas
+
 from decimal import Decimal
-
-from analises import faturamento_total, faturamento_por_produto, faturamento_por_vendedor
-
 
 def test_faturamento_total():
     resultado = faturamento_total()
@@ -26,3 +25,31 @@ def test_faturamento_por_vendedor():
         ("Vendedor 2", Decimal("998.00")),
         ("Vendedor 3", Decimal("639.20"))
     ]   
+
+def test_faturamento_total_pandas():
+    resultado = faturamento_total_pandas()
+
+    assert resultado == 2725.00 
+
+def test_maior_venda():
+    resultado = maior_venda()
+
+    assert resultado["cliente"] == "Cliente D"
+    assert resultado["produto"] == "Produto Y"
+    assert resultado["vendedor"] == "Vendedor 2"
+    assert resultado["quantidade"] == 15
+    assert resultado["faturamento"] == 748.50
+
+def test_faturamento_por_produto_pandas():
+    resultado = faturamento_por_produto_pandas()
+
+    assert resultado["Produto X"] == Decimal("1087.80")
+    assert resultado["Produto Y"] == Decimal("998.00")
+    assert resultado["Produto Z"] == Decimal("639.20")
+
+def test_faturamento_por_vendedor_pandas():
+    resultado = faturamento_por_vendedor_pandas()
+
+    assert resultado["Vendedor 1"] == Decimal("1087.80")
+    assert resultado["Vendedor 2"] == Decimal("998.00")
+    assert resultado["Vendedor 3"] == Decimal("639.20")
